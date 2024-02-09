af4c8f3e8a46dfccdd1e37e9f0d6d73ea889525f
Nader Saoudy <developernader@gmail.com>
2024-02-10 00:29:37 +0300

    using AddRange and Remove

--- ConsoleApp_LINQ/Program.cs
+++ ConsoleApp_LINQ/Program.cs
@@ -1,13 +1,22 @@
 ﻿List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-List<string> stringList = new List<string>() { "Nader", "Yasser", "Mo salah", "Rashad" };
-var result = Enumerable.Where(numbers, n => n > 5);
-foreach (var item in result)
+List<string> stringList = new List<string>() 
+{ "Nader", "Yasser", "Mo salah", "Rashad","ahmed ahmed","ahmed mohamady" };
+//var result = Enumerable.Where(numbers, n => n > 5);
+//foreach (var item in result)
+//{
+//    Console.WriteLine(item);
+//}
+//Console.WriteLine("----------------------------------------------------------------");
+//var numberResult = numbers.Where(n => n > 5);
+//foreach (var item in numberResult)
+//{
+//    Console.WriteLine(item);
+//}
+
+var names = stringList.Where(x => x.Contains("ahmed"));
+foreach (var item in names)
 {
-    Console.WriteLine(item);
+   Console.WriteLine(item);
 }
-Console.WriteLine("----------------------------------------------------------------");
-var numberResult = numbers.Where(n => n > 5);
-foreach (var item in numberResult)
-{
-    Console.WriteLine(item);
-}
+
+stringList.AddRange(new string[] { "saeed", "ahmed morad", "ahmed ahmed" });
